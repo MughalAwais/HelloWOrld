eb5d22933332d2afa71b945a6cbe4a56432b6118
Awais Mughal <awsmghl@gmail.com>
2022-03-07 19:25:31 +0500

    hello

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textView= (TextView) findViewById(R.id.tv);
+        textView.setText("Welcome to android Development!!");
+
+
+    }
+}
